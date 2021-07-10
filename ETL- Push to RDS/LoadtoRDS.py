7fedf55acf313c7dfc88d2bcbf0a8d99ab80843b
Saurabh Khanolkar <40240678+saurabhkhanolkar@users.noreply.github.com>
2021-07-10 13:27:17 +0530

    Update LoadtoRDS.py

--- ETL- Push to RDS/LoadtoRDS.py
+++ ETL- Push to RDS/LoadtoRDS.py
@@ -5,9 +5,9 @@ import pandas as pd
 from sqlalchemy import delete
 from sqlalchemy.sql import text
 
-engine=create_engine('mysql+pymysql://admin:12345678@database-1.cod1kdbr9qur.us-east-2.rds.amazonaws.com/Finaldb',echo=False)
+engine=create_engine('RDS Endpoint/DBname',echo=False)
 dfglobal=t.getdataglobal()
-dfglobal.rename(columns = {'key': 'key1'}, inplace = True)
+dfglobal.rname(columns = {'key': 'key1'}, inplace = True)
 mycon=engine.connect()
 
 
@@ -22,73 +22,3 @@ mycon.execute(delete_stmt)
 #d.execute()
 
 dfglobal.to_sql('GLOBALCURRENT',mycon,index = False,if_exists='append')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
-engine=create_engine('mysql+pymysql://admin:12345678@database-1.cod1kdbr9qur.us-east-2.rds.amazonaws.com/trialagain',echo=False)
-
-dfindia=t.getdataindia()
-dfglobal=tg.getdataglobal()
-#print(dfindia)
-#print(dfglobal)
-dfindia.rename(columns = {'key': 'key1'}, inplace = True)
-dfglobal.rename(columns = {'key': 'key1'}, inplace = True)
-#print(dfindia)
-#print(dfglobal)
-mycon=engine.connect()
-
-#dfindia.to_sql('INDIATRACKSFINAL1',mycon,index = False,if_exists='append')
-dfglobal.to_sql('GLOBALTRACKSFINAL',mycon,index = False,if_exists='append')
-
-
-
-
-
-import pandas as pd
-df=pd.read_sql_table('GLOBALTRACKSFINAL',engine)
-print(df)
-"""
